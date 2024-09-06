fe51120ef202e6d6e3bb5ed24832a521e8be971b
Muriel Pereira <83676196+murielpereira@users.noreply.github.com>
2024-09-06 20:09:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,46 @@
+let dados = [
+    {
+        titulo : "Lionel Messi",
+        tags : "barcelona psg bola de ouro melhor do mundo",
+        descricao : "Lionel Messi é considerado por muitos o maior jogador de futebol de todos os tempos. Com sua habilidade excepcional, visão de jogo impecável e dribles mágicos, o argentino conquistou o mundo com sua arte nos gramados. Ao longo de sua carreira, Messi acumulou inúmeros títulos e recordes, incluindo diversas Bolas de Ouro e Chuteiras de Ouro. Sua trajetória, marcada por momentos de pura magia e paixão pelo esporte, o transformou em um ídolo global e um dos maiores embaixadores do futebol. Atualmente, Messi continua encantando fãs ao redor do mundo com seu talento e carisma.",
+        link : "https://www.instagram.com/leomessi/",
+        foto : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-a5RgBtzrpyEoYwIM3ErK3Zdx1BWAE9DMcNab1rDwYchCJkzh",
+    },
+    {
+        titulo : "Cristiano Ronaldo",
+        tags : "manchester united juventus real madrid",
+        descricao : "Cristiano Ronaldo é outro ícone do futebol mundial, conhecido por sua força física, habilidade de arremate e gols espetaculares. Com uma carreira marcada por títulos em diversos clubes europeus, Ronaldo também se destaca por sua personalidade forte e determinação em campo. Sua rivalidade com Messi o tornou um dos atletas mais populares do mundo.",
+        link : "https://www.instagram.com/cristiano/",
+    },
+    {
+        titulo : "Ronaldinho Gaúcho",
+        tags : "milan barcelona grêmio",
+        descricao : "Ronaldinho Gaúcho é considerado um dos maiores gênios do futebol brasileiro. Sua habilidade com a bola, dribles desconcertantes e alegria dentro de campo o tornaram um dos jogadores mais queridos da história do esporte. Campeão da Copa do Mundo de 2002 com a Seleção Brasileira, Ronaldinho encantou o mundo com seu futebol mágico.",
+        link : "https://www.instagram.com/ronaldinho/",
+    },
+    {
+        titulo: "Rivaldo",
+        tags : "barcelona seleção copa do mundo",
+        descricao: "Considerado um dos maiores meias da história do futebol, Rivaldo encantou o mundo com sua habilidade técnica e visão de jogo. Campeão mundial em 2002, ele foi fundamental para a conquista do pentacampeonato brasileiro. Sua famosa falta contra a Turquia na Copa de 2002 é um dos momentos mais icônicos do futebol mundial.",
+        link: "https://www.instagram.com/rivaldo" // Link fictício, ajuste para o link correto
+    },
+    {
+        titulo: "Roberto Carlos",
+        tags : "",
+        descricao: "Roberto Carlos é um dos maiores laterais esquerdos da história do futebol. Famoso por seus chutes potentes e precisos, ele foi fundamental para a conquista da Copa do Mundo de 2002 pela Seleção Brasileira.",
+        link: "https://www.instagram.com/robertocarlosoficial/",
+    },
+    {
+        titulo: "Ronaldo Fenômeno",
+        tags : "gordo",
+        descricao: "Ronaldo Fenômeno é considerado um dos maiores atacantes de todos os tempos. Sua velocidade, habilidade e faro de gol o tornaram um dos artilheiros mais prolíficos da história do futebol. Campeão da Copa do Mundo de 2002, Ronaldo encantou o mundo com seus gols memoráveis.",
+        link: "https://www.instagram.com/ronaldo/",
+    },
+    {
+        titulo: "Kaká",
+        tags : "kaka futebolista seleção milan real madrid",
+        descricao: "Kaká é um dos maiores meias da história do futebol brasileiro. Conhecido por sua visão de jogo, passes precisos e gols importantes, ele foi fundamental para a conquista da Copa do Mundo de 2002 e da Champions League com o Milan. Em 2007, foi eleito o melhor jogador do mundo pela FIFA.",
+        link: "https://www.instagram.com/kaka/",
+    }
+];
+
---
+++ script.js
@@ -0,0 +1,58 @@
+// console.log(dados[1].titulo);
+
+function pesquisar() {
+    // Essa função é responsável por realizar a pesquisa e atualizar a interface com os resultados.
+
+    let section = document.getElementById("resultados-pesquisa");
+    // Seleciona o elemento HTML com o ID "resultados-pesquisa" e armazena-o na variável 'section'.
+    // Esse elemento provavelmente é uma seção da página onde os resultados serão exibidos.
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p>Nenhuma pesquisa realizada</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa.
+    // Essa string será preenchida com o HTML que será inserido na seção.
+
+    for (let dado of dados) {
+
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += 
+            `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Instagram</a>
+            </div>
+            `
+        }
+
+        if (!resultados) {
+            resultados = "<p>Nada foi encontrado</p>"
+        }
+    }
+        // Inicia um loop 'for...of' para percorrer cada item (dado) dentro do array 'dados'.
+        // A cada iteração, a variável 'dado' receberá o valor do próximo item do array.
+        // Dentro do loop, é construída uma string de HTML para cada resultado da pesquisa.
+        // A string é concatenada à variável 'resultados' usando template literals (``).
+        // As propriedades 'titulo', 'descricao' e 'link' do objeto 'dado' são inseridas no HTML usando interpolação de strings.
+        
+    section.innerHTML = resultados;
+    // Após o loop, o conteúdo HTML da variável 'resultados' é atribuído ao atributo 'innerHTML' do elemento 'section'.
+    // Isso substitui o conteúdo anterior da seção pelos novos resultados da pesquisa.
+}
